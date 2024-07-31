4c1aad9005242dfaae944d7aa3ec4dc07eeb22a7
skywalkerx64 <rickjoaodonald@gmail.com>
2024-07-31 12:06:07 +0100

    wip

--- app/Services/PromptBuilder/DataProcessor.php
+++ app/Services/PromptBuilder/DataProcessor.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 class  DataProcessor
 {
-    const GEMINI_ENGINE_API_URL = 'http://localhost:3333/projects/process';
+    const GEMINI_ENGINE_API_URL = 'http://159.203.86.163:3333/projects/process';
     public static function getProcessedData($project, $new_budget, $new_duration)
     {
         $response = Http::withOptions(['timeout' => 1200])->post(self::GEMINI_ENGINE_API_URL, [
