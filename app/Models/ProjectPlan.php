878478a8fcbe2a03a4fa3535c4e7afd19c65e4e8
skywalkerx64 <rickjoaodonald@gmail.com>
2025-02-18 18:34:59 +0100

    wip

--- app/Models/ProjectPlan.php
+++ app/Models/ProjectPlan.php
@@ -84,6 +84,7 @@ class ProjectPlan extends Model
         'environment' => 'array',
         'partners_reinforcement' => 'array',
         'outter_strategies' => 'array',
+        'gestion_strategy' => 'array',
     ];
 
     protected $appends = [
